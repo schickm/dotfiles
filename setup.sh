43468a79934cc7c66acfce412fbaa6a635df49f6
Matt Schick <matt@schickm.com>
2017-12-04 17:26:57 -0800

    add setup file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/bin
+mkdir -p ~/.config/kak
+mkdir -p ~/.ssh
+
+ln -s ~/vc/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/vc/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/vc/dotfiles/kakrc ~/.config/kak/kakrc
+ln -s ~/vc/dotfiles/ssh/config ~/.ssh/config
