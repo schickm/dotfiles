9df1b2f255625bf78750669e621548b78ca238ea
Matt Schick <matt@schickm.com>
2019-05-15 08:02:43 -0500

    Add nodejs mpforumsearch script

---
+++ scripts/mpforumsearch/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const wrap = require('word-wrap');
+const screenCols = process.stdout.columns;
+
+const query = process.argv[2];
+const url=`https://www.mountainproject.com/ajax/public/search/results/category?q=${query}&c=Forums&o=0&s=Newest`
+
+const main = async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+  json.results.Forums.forEach(printResult);
+};
+
+const printResult = (html) => {
+  const $ = cheerio.load(html);
+  const url = 'https://www.mountainproject.com' + $('a').attr('href');
+	const data = $('body').text().split("\n").map(x => x.trim()).filter(x => x !== '');
+	const [title, timestamp] = data.slice(0, 2);
+	const match = data.slice(2).join(' ');
+
+	console.log(`${title + timestamp.padStart(screenCols - title.length)}
+${wrap(match, {width: screenCols})}
+${url}
+`);
+};
+
+main();
+
