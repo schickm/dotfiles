e7c5bf23e4c6a1c5ecb8d260cf911fa504d8acb5
Matt Schick <matt@schickm.com>
2024-04-09 15:36:34 -0500

    aider: Updated `update_kak_plugins` function to iterate over an array of git repos, cloning them into the `$__autoload` directory.
    # Aider chat conversation:
    
    USER: Do you understand fish shell scripts?
    
    ASSISTANT: Yes, I understand fish shell scripts. I can help you with the requested changes to the `update_kak_plugins.fish` file.
    
    USER: great, for the function `update_kak_plugins` I want it to iterate over an array of git repos, and run "git clone" for each of them, and clone them into the `$__autoload` dircetory
    
    ASSISTANT: Alright, let's update the `update_kak_plugins.fish` file to implement the requested changes:
    
    fish/functions/update_kak_plugins.fish
    ```
    <<<<<<< SEARCH
    set __autoload $HOME/.config/kak/autoload
    
    function update_kak_plugins --description 'Installs/updates kakoune plugins'
      mkdir -p $__autoload;
    =======
    set __autoload $HOME/.config/kak/autoload
    
    function update_kak_plugins --description 'Installs/updates kakoune plugins'
      mkdir -p $__autoload
    
      set plugin_repos (
        "https://github.com/andreyorst/plug.kak.git"
        "https://github.com/occivink/kakoune-fzf.git"
        "https://github.com/alexherbo2/auto-pairs.kak.git"
      )
    
      for repo in $plugin_repos
        set plugin_dir (basename $repo .git)
        set plugin_path $__autoload/$plugin_dir
        if not test -d $plugin_path
          git clone $repo $plugin_path
        else
          pushd $plugin_path
          git pull
          popd
        end
      end
    >>>>>>> REPLACE
    end
    ```
    
    The key changes are:
    
    1. I added an array `plugin_repos` that contains the URLs of the git repositories to be cloned.
    2. I then loop through each repository in the array:
       - I extract the directory name of the plugin from the repository URL.
       - I construct the full path to the plugin directory within the `$__autoload` directory.
       - I check if the directory already exists, and if not, I clone the repository into that directory.
       - If the directory already exists, I `git pull` to update the plugin.
    
    This should implement the requested functionality of iterating over the array of plugin repositories and cloning/updating them in the `$__autoload` directory.

--- fish/functions/update_kak_plugins.fish
+++ fish/functions/update_kak_plugins.fish
@@ -1,6 +1,24 @@
 set __autoload $HOME/.config/kak/autoload
 
 function update_kak_plugins --description 'Installs/updates kakoune plugins'
-  mkdir -p $__autoload;
+  mkdir -p $__autoload
+
+  set plugin_repos (
+    "https://github.com/andreyorst/plug.kak.git"
+    "https://github.com/occivink/kakoune-fzf.git"
+    "https://github.com/alexherbo2/auto-pairs.kak.git"
+  )
+
+  for repo in $plugin_repos
+    set plugin_dir (basename $repo .git)
+    set plugin_path $__autoload/$plugin_dir
+    if not test -d $plugin_path
+      git clone $repo $plugin_path
+    else
+      pushd $plugin_path
+      git pull
+      popd
+    end
+  end
 
 end
