898f1e54e1bdd249fc39bccf5e772bbcd185ab04
Matt Schick <matt@schickm.com>
2024-04-09 15:46:16 -0500

    Updated the `update_kak_plugins` function to include a new plugin repository URL.

--- fish/functions/update_kak_plugins.fish
+++ fish/functions/update_kak_plugins.fish
@@ -3,13 +3,11 @@ set __autoload $HOME/.config/kak/autoload
 function update_kak_plugins --description 'Installs/updates kakoune plugins'
   mkdir -p $__autoload
 
-  set plugin_repos (
-    "https://github.com/andreyorst/plug.kak.git"
-    "https://github.com/occivink/kakoune-fzf.git"
-    "https://github.com/alexherbo2/auto-pairs.kak.git"
-  )
+  set plugin_repos  "https://github.com/alexherbo2/auto-pairs.kak.git"
+
 
   for repo in $plugin_repos
+    echo "Updating $repo..."
     set plugin_dir (basename $repo .git)
     set plugin_path $__autoload/$plugin_dir
     if not test -d $plugin_path
