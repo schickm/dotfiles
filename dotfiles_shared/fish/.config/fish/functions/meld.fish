37a4ba21ce0857c418f4b81d4a0e61a2898ecee8
Matt Schick <schickm@gmail.com>
2026-01-09 20:57:50 -0600

    Remove old macos meld wrapper

--- dotfiles_shared/fish/.config/fish/functions/meld.fish
+++
@@ -1,4 +0,0 @@
-function meld --wraps=/Applications/Meld.app/Contents/MacOS/Meld --description 'alias meld=/Applications/Meld.app/Contents/MacOS/Meld'
-  /Applications/Meld.app/Contents/MacOS/Meld $argv
-        
-end
